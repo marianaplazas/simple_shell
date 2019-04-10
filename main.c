a95f22fbb379debf23c5c1a85268ad1b498e1876
Mariana <marianaplazas@live.com>
2019-04-10 21:45:30 +0000

    [features] path, header, strtok

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "shell.h"
+int main(void)
+{
+	char *line = NULL;
+	size_t len = 0;
+	char **token;
+	int status;
+	
+	while (1) 
+	{
+		printf("$ ");
+		status = getline(&line, &len, stdin);
+		token = _strtok(line);
+		if (token[0] == NULL)
+			perror("no such a file or directory");
+	}
+	free(line);
+	exit(98);
+	return(0);
+}
---
+++ shell.h
@@ -0,0 +1,9 @@
+#ifndef SHELL_H
+#define SHELL_H
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+char **_strtok(char *istring);
+int hand_errors(char **av);
+#endif
