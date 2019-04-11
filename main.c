712901b70e25443fb01a0c3a6fa493aae48a03c0
Mariana <marianaplazas@live.com>
2019-04-11 17:59:25 +0000

    [fixes] main, with a fork]

--- main.c
+++ main.c
@@ -13,10 +13,31 @@ int main(void)
 	{
 		printf("$ ");
 		/* status =*/ getline(&line, &len, stdin);
+		/**
+		 *aqui debe ir el manejador de errores
+		 *usar continue
+		 */
 		token = _strtok(line);
-		if (token[0] == NULL)
-			perror("no such a file or directory");
-		path();
+
+		if (token[0] == NULL
+		    perror("no such a file or directory");
+
+		if (file_exist(token) != 0)
+			path(token[0]);
+
+		if (fork() == 0)
+		{
+			if (execve(token[0], token, NULL) == -1)
+				/**
+				 *perror
+				 */
+				break;
+		}
+		else
+		{
+			wait(NULL)
+		}
+
 	}
 	free(line);
 	exit(98);
