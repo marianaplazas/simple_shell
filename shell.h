450f73092f0a816fc53494a5c5fcaa36b8339677
Mariana <marianaplazas@live.com>
2019-04-12 02:00:19 +0000

    fixes, strtok shell

--- shell.h
+++ shell.h
@@ -1,6 +1,5 @@
 #ifndef SHELL_H
 #define SHELL_H
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -8,11 +7,10 @@
 #include <sys/wait.h>
 #include <errno.h>
 #include <unistd.h>
-
 int _strcmp(char *s1, char *s2);
-char **_strtok(char *istring);
+char **_strtok(char *istring, const char *delimit);
 int hand_errors(char **av);
 void path();
 int cont_word(char *palabra);
-
+char* strcp(char *str);
 #endif
